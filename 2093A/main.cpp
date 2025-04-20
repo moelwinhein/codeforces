f545beaef6688671e98a6cba1e33fbf100fb6ef9
Moe Lwin Hein <hein.moelwin@gmail.com>
2025-04-20 18:31:30 +0800

    Add 2093A

---
+++ 2093A/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+#if fileio
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc; cin >> tc;
+    while (tc--) {
+        int k; cin >> k;
+
+        cout << (k & 1 ? "YES" : "NO") << '\n';
+    }
+
+    return 0;
+}
